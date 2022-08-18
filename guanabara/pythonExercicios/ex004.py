9b7b1cc345a59949a0b304e20a1a211ba81dfc96
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-08-18 17:28:47 -0300

    melhorias no exercicio e novas funções

--- guanabara/pythonExercicios/ex004.py
+++ guanabara/pythonExercicios/ex004.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
 #faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele
+
 n = input('Digite algo: ')
-print(n)
-print(type(n))
-print(n.isnumeric())
-print(n.isalpha())
-print(n.isalnum())
+print('O tipo primitivo desse valor é', type(n))
+print("Só tem espaços?",n.isspace())
+print("É um número?",n.isnumeric())
+print("É alfabético?",n.isalpha())
+print("É alfanumérico?",n.isalnum())
+print("Está em maiúsculas?",n.isupper())
+print("Está em minúsculas?",n.islower())
+print("Está capitalizada?",n.istitle())
