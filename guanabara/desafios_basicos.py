1d73f999297843cef00e8b21fd114995d66d177f
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-08-18 12:06:50 -0300

    adiciona primeiros exercicios

---
+++ guanabara/desafios_basicos.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#desafio1
+#nome = input('Qual é o seu nome?')
+#print('Ola', nome,"! Prazer em te conhecer!")
+
+#desafio2
+#dia = input("Dia = ")
+#mes = input("Mês = ")
+#ano = input("Ano = ")
+#print("Voce nasceu no dia",dia,"de",mes,"de",ano,". Correto?")
+
+#desafio3
+primeiro_numero = input('Primeiro número:')
+segundo_numero = input('Segundo número:')
+print("A soma é", primeiro_numero+segundo_numero)
+
---
+++ guanabara/primeiros-comandos.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+print('Olá mundo!')
+print(7+4)
+print("7"+"4")
+
+nome = 'Guanabara'
+idade = 25
+peso = 74.8
+print(nome,idade,peso)
+
+nome = input('Qual é o seu nome?')
+idade = input('Qual a sua idade?')
+peso = input('Qual o seu peso?')
+print(nome,idade,peso)
